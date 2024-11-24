ebe5ec7422764650026dc3dc3a72ff2dff831dfa
Sabli Jr <sablijr@yahoo.com>
2024-11-24 09:10:17 +0100

    Finishing the section

--- client/src/Components/myWork/index.jsx
+++ client/src/Components/myWork/index.jsx
@@ -5,19 +5,50 @@ import Wishties from "../../Assets/_wishties.png";
 import Sponsorwave from "../../Assets/_sponsorwave.png";
 import ChatFolderz from "../../Assets/_chatfolderz.png";
 
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { LuPauseCircle } from "react-icons/lu";
+
 const Index = () => {
   return (
-    <section className='workSection'>
-      <h3 className='workTitle'>My Startup Projects.</h3>
-      <div className='workDiv'>
-        <div>
-          <img src='' alt='' className='_project_logo' />
+    <section className='_work_section'>
+      <h3 className='_work_title'>My Startup Projects.</h3>
+      <div className='_work_container'>
+        <div className='_project_container'>
+          <img src={Sponsorwave} alt='' className='_project_logo _extrac' />
+          <p>
+            Helping Creators Make a Living Doing what they love while enabling
+            companies to reach their users!
+          </p>
+          <button>
+            <a target='#blank' href='https://www.sponsorwave.com/'>
+              <HiOutlineExternalLink /> sponsorwave.com
+            </a>
+          </button>
         </div>
-        <div>
-          <img src='' alt='' className='_project_logo' />
+        <div className='_project_container'>
+          <img src={ChatFolderz} alt='' className='_project_logo' />
+          <p>
+            Organize your AI conversations with folders, search, bookmark, and
+            manage prompts, prompt library and more... All in one extension.
+          </p>
+          <button>
+            <a target='#blank' href='https://chatfolderz.com/'>
+              <HiOutlineExternalLink /> chatfolderz.com
+            </a>
+          </button>
         </div>
-        <div>
-          <img src='' alt='' className='_project_logo' />
+        <div className='_project_container'>
+          <img src={Wishties} alt='' className='_project_logo' />
+          <p>
+            Your fan’s wish links and your wish list links all in one place.
+            Stop confusing your fans, make it easier for them to support you!
+          </p>
+          <button id='_discontinued'>
+            <a target='#blank' href='https://www.wishties.com/'>
+              <LuPauseCircle />
+              discontinued
+            </a>
+          </button>
         </div>
       </div>
     </section>
