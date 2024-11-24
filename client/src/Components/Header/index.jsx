bad66b376874746456e9a611a35ddc1aa21fcfb5
Sabli Jr <sablijr@yahoo.com>
2024-11-24 09:59:54 +0100

    Removing unnecessary stuff

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -2,50 +2,11 @@ import React from "react";
 import "./header.css";
 
 import Logo from "../../Assets/Logo.png";
-import { ImArrowUp2 } from "react-icons/im";
-import { Link as ScrollLink } from "react-scroll";
 
 const Header = () => {
   return (
     <header className='Header'>
       <img src={Logo} alt='' className='logo' />
-      {/* <div className='navLinksDiv'>
-        <ScrollLink
-          to='Header'
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}>
-          <ImArrowUp2 className='upBtn' />
-        </ScrollLink>
-        <nav className='navLinks'>
-          <ScrollLink
-            to='aboutSection'
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}>
-            About
-          </ScrollLink>
-          <ScrollLink
-            to='workSection'
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}>
-            Work
-          </ScrollLink>
-        </nav>
-        <ScrollLink
-          className='resume'
-          to='contact'
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}>
-          Contact
-        </ScrollLink>
-      </div> */}
     </header>
   );
 };
